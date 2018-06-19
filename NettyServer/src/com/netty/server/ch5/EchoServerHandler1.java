80de166be004efcb18eb903dc8a6e85b3aa6caf8
guozhk <1565619659@qq.com>
2018-06-19 11:39:44 +0800

    FixedLengthFrameDecoder  解码器

---
+++ NettyServer/src/com/netty/server/ch5/EchoServerHandler1.java
@@ -0,0 +1,26 @@
+package com.netty.server.ch5;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class EchoServerHandler1 extends ChannelHandlerAdapter {
+
+    int counter = 0;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String) msg;
+        System.out.println(" This is " + ++counter + " times receive client : [" + body + "$_]");
+      /*  body += "$_";
+        ByteBuf echo = Unpooled.copiedBuffer(body.getBytes());
+        ctx.writeAndFlush(echo);*/
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
